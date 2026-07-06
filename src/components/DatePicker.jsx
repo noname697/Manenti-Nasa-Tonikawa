5b647d1b6e0762507852e53bb47b53b625452e2f
Arthur Manenti <arthur.manenti.videira@gmail.com>
2026-07-05 22:13:40 -0300

    fix: Ajustes mínimos

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -5,7 +5,7 @@ const DatePicker = ({ value, onChange }) => {
       value={value}
       max={new Date().toISOString().split("T")[0]}
       onChange={onChange}
-      className="rounded-full border border-white/10 bg-white/10 px-5 py-3 text-text backdrop-blur-md transition hover:bg-white/20"
+      className="rounded-full border border-white/10 bg-surface/80 px-5 py-3 text-text backdrop-blur-md transition hover:bg-white/20 outline-none focus:border-primary"
     />
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,20 @@ import { useState } from "react";
 import { useApod } from "../hooks/useApod";
 import { APODCard } from "./APODCard";
 import DatePicker from "./DatePicker";
+import { getRandomDate } from "../services/nasaApi";
+import RandomButton from "./RandomButton";
 
 export const Hero = () => {
   const [selectedDate, setSelectedDate] = useState("");
+
+  const handleRandomDate = () => {
+    let randomDate;
+
+    do {
+      randomDate = getRandomDate();
+    } while (randomDate === selectedDate);
+    setSelectedDate(randomDate);
+  };
 
   const { picture, loading, error } = useApod(selectedDate);
 
@@ -23,16 +34,17 @@ export const Hero = () => {
       </section>
     );
   }
-  const handler = (e) => {
+  const handleChangeDate = (e) => {
     console.log(e.target.value);
-    
+
     setSelectedDate(e.target.value);
   };
 
   return (
     <section className="space-y-8">
-      <div className="mx-auto flex max-w-7xl justify-end px-8">
-        <DatePicker value={selectedDate} onChange={handler} />
+      <div className="mx-auto flex max-w-7xl items-center justify-end gap-4 px-8">
+        <RandomButton onCLick={handleRandomDate} />
+        <DatePicker value={selectedDate} onChange={handleChangeDate} />
       </div>
       <div className="max-auto max-w-7xl px-8">
         <APODCard picture={picture} />
